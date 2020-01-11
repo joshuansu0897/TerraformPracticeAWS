effb3d5466c189bcaf3302010196e57cbcf5383e
Joshua Saucedo <joshuansu0897@gmail.com>
2020-01-10 20:02:11 -0700

    finished vpc module

--- modules/vps/output.tf
+++ modules/vps/output.tf
@@ -1,14 +1,14 @@
 output "vpc_main_id" {
-  value = aws_vpc.main.id
+  value       = aws_vpc.main.id
   description = "Is the ID of main VPC"
 }
 
 output "public_subnets" {
-  value = aws_subnet.public_subnets
+  value       = aws_subnet.public_subnets
   description = "Is the list of publics subnets"
 }
 
 output "private_subnets" {
-  value = aws_subnet.private_subnets
+  value       = aws_subnet.private_subnets
   description = "Is the list of privates subnets"
 }
